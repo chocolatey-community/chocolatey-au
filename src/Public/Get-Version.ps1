cbfbd312cda82b52a62cba5b9c9fd27b584b858c
Thomas Démoulins <tdemoulins@gmail.com>
2023-06-18 01:22:36 +0200

    feat: support all semver versions

--- src/Public/Get-Version.ps1
+++ src/Public/Get-Version.ps1
@@ -1,11 +1,11 @@
-# Author: Thomas DÃ©moulins <tdemoulins@gmail.com>
+# Author: Thomas Démoulins <tdemoulins@gmail.com>
 
 <#
 .SYNOPSIS
-    Parses a semver-like object from a string in a flexible manner.
+    Parses a SemVer-like object from a string in a flexible manner.
 
 .DESCRIPTION
-    This function parses a string containing a semver-like version
+    This function parses a string containing a SemVer-like version
     and returns an object that represents both the version (with up to 4 parts)
     and optionally a pre-release and a build metadata.
 
@@ -16,26 +16,48 @@
     - extra spaces are ignored
     - optional delimiters can be provided to help parsing the string
 
-.EXAMPLE
-    Get-Version 'Last version: 1.2.3 beta 3.'
+    Parameter -SemVer allows to specify the max supported SemVer version:
+    V1 (default) or V2 (requires choco v2.0.0). EnhancedV2 is about
+    transforming a SemVer1-like version into a SemVer2-like one when
+    possible (e.g. 1.61.0-beta.0 instead of 1.61.0-beta0).
 
-    Returns 1.2.3-beta.3
-
-.EXAMPLE
-    Get-Version 'https://github.com/atom/atom/releases/download/v1.24.0-beta2/AtomSetup.exe'
-
-    Return 1.24.0-beta.2
+    Resulting version is normalized the same way chocolatey/nuget does.
+    See https://learn.microsoft.com/en-us/nuget/concepts/package-versioning#normalized-version-numbers
 
 .EXAMPLE
-    Get-Version 'http://mirrors.kodi.tv/releases/windows/win32/kodi-17.6-Krypton-x86.exe' -Delimiter '-'
+    Get-Version 'Current version 2.1.1 beta 2.'
 
-    Return 17.6
+    Returns 2.1.1-beta2
+
+.EXAMPLE
+    Get-Version -SemVer V2 'Current version 2.1.1 beta 2.'
+
+    Returns 2.1.1-beta.2
+
+.EXAMPLE
+    Get-Version -SemVer V2 '4.0.3Beta1'
+
+    Returns 4.0.3-Beta1
+
+.EXAMPLE
+    Get-Version -SemVer EnhancedV2 '4.0.3Beta1'
+
+    Returns 4.0.3-Beta.1
+
+.EXAMPLE
+    Get-Version 'https://dl.airserver.com/pc32/AirServer-5.6.3-x86.msi' -Delimiter '-'
+
+    Returns 5.6.3
 #>
 function Get-Version {
     [CmdletBinding()]
     param(
+        # Supported SemVer version: V1 (default) or V2 (requires choco v2.0.0).
+        # EnhancedV2 allows to transform a SemVer1-like version into SemVer2-like one (e.g. 1.2.0-rc.3 instead of 1.2.0-rc3)
+        [ValidateSet('V1', 'V2', 'EnhancedV2')]
+        [string] $SemVer = 'V1',
         # Version string to parse.
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory, Position=0)]
         [string] $Version,
         # Optional delimiter(s) to help locate the version in the string: the version must start and end with one of these chars.
         [char[]] $Delimiter
@@ -46,10 +68,10 @@ function Get-Version {
         $regex = $Version | Select-String -Pattern "[$delimiters](\d+\.\d+[^$delimiters]*)[$delimiters]" -AllMatches
         foreach ($match in $regex.Matches) {
             $reference = [ref] $null
-            if ([AUVersion]::TryParse($match.Groups[1], $reference, $false)) {
+            if ([AUVersion]::TryParse($match.Groups[1], $reference, $false, $SemVer)) {
                 return $reference.Value
             }
         }
     }
-    return [AUVersion]::Parse($Version, $false)
+    return [AUVersion]::Parse($Version, $false, $SemVer)
 }
--- tests/Get-Version.Tests.ps1
+++ tests/Get-Version.Tests.ps1
@@ -1,129 +1,75 @@
 remove-module Chocolatey-AU -ea ignore
 import-module $PSScriptRoot\..\Chocolatey-AU\Chocolatey-AU.psm1 # Tests require the private functions exported
 
-Describe 'ConvertTo-AUVersion' -Tag getversion {
+Describe 'Get-Version' -Tag version {
     InModuleScope Chocolatey-AU {
-        $testCases = @(
-            @{Value = '01.02.03.04-beta.01+xyz.01'; ExpectedVersion = '1.2.3.4'; ExpectedPrerelease = 'beta.1'; ExpectedBuildMetadata = 'xyz.01'}
-            @{Value = '01.02.03-beta01+xyz01'     ; ExpectedVersion = '1.2.3'  ; ExpectedPrerelease = 'beta01'; ExpectedBuildMetadata = 'xyz01'}
-            # The following test cases are intended to match chocolatey normalized versions
-            @{Value = '01.02-beta+xyz'            ; ExpectedVersion = '1.2.0'  ; ExpectedPrerelease = 'beta'  ; ExpectedBuildMetadata = 'xyz'}
-            @{Value = '01.02.03.00-beta+xyz'      ; ExpectedVersion = '1.2.3'  ; ExpectedPrerelease = 'beta'  ; ExpectedBuildMetadata = 'xyz'}
-        )
-
-        It 'should convert a strict version: <value>' -TestCases $testCases { param([string] $Value, [version] $ExpectedVersion, [string] $ExpectedPrerelease, [string] $ExpectedBuildMetadata)
-            $res = ConvertTo-AUVersion $Value
-            $res | Should Not BeNullOrEmpty
-            $res.Version | Should Be $ExpectedVersion
-            $res.Prerelease | Should BeExactly $ExpectedPrerelease
-            $res.BuildMetadata | Should BeExactly $ExpectedBuildMetadata
-            $res.ToString() | Should BeExactly "$ExpectedVersion-$ExpectedPrerelease+$ExpectedBuildMetadata"
-            $res.ToString(2) | Should BeExactly $ExpectedVersion.ToString(2)
-            $res.ToString(-1) | Should BeExactly $ExpectedVersion.ToString()
-        }
-
-        $testCases = @(
-            @{Value = '1.2.3.4a'}
-            @{Value = 'v1.2.3.4-beta.1+xyz.01'}
-        )
-
-        It 'should not convert a non strict version: <value>' -TestCases $testCases { param([string] $Value)
-            { ConvertTo-AUVersion $Value } | Should Throw
-        }
-
-        $testCases = @(
-            @{A = '1.9.0'           ; B = '1.9.0'           ; ExpectedResult = '='}
-            @{A = '1.9.0'           ; B = '1.10.0'          ; ExpectedResult = '<'}
-            @{A = '1.10.0'          ; B = '1.11.0'          ; ExpectedResult = '<'}
-            @{A = '1.0.0'           ; B = '2.0.0'           ; ExpectedResult = '<'}
-            @{A = '2.0.0'           ; B = '2.1.0'           ; ExpectedResult = '<'}
-            @{A = '2.1.0'           ; B = '2.1.1'           ; ExpectedResult = '<'}
-            @{A = '1.0.0-alpha'     ; B = '1.0.0-alpha'     ; ExpectedResult = '='}
-            @{A = '1.0.0-alpha'     ; B = '1.0.0'           ; ExpectedResult = '<'}
-            @{A = '1.0.0-alpha.1'   ; B = '1.0.0-alpha.1'   ; ExpectedResult = '='}
-            @{A = '1.0.0-alpha.1'   ; B = '1.0.0-alpha.01'  ; ExpectedResult = '='}
-            @{A = '1.0.0-alpha'     ; B = '1.0.0-alpha.1'   ; ExpectedResult = '<'}
-            @{A = '1.0.0-alpha.1'   ; B = '1.0.0-alpha.beta'; ExpectedResult = '<'}
-            @{A = '1.0.0-alpha.beta'; B = '1.0.0-beta'      ; ExpectedResult = '<'}
-            @{A = '1.0.0-beta'      ; B = '1.0.0-beta.2'    ; ExpectedResult = '<'}
-            @{A = '1.0.0-beta.2'    ; B = '1.0.0-beta.11'   ; ExpectedResult = '<'}
-            @{A = '1.0.0-beta.11'   ; B = '1.0.0-rc.1'      ; ExpectedResult = '<'}
-            @{A = '1.0.0-rc.1'      ; B = '1.0.0'           ; ExpectedResult = '<'}
-            @{A = '1.0.0'           ; B = '1.0.0+1'         ; ExpectedResult = '='}
-            @{A = '1.0.0+1'         ; B = '1.0.0+2'         ; ExpectedResult = '='}
-            @{A = '1.0.0-alpha'     ; B = '1.0.0-alpha+1'   ; ExpectedResult = '='}
-            @{A = '1.0.0-alpha+1'   ; B = '1.0.0-alpha+2'   ; ExpectedResult = '='}
-        )
-
-        It 'should compare 2 versions successfully: <a> <expectedResult> <b>' -TestCases $testCases { param([string] $A, [string] $B, [string] $ExpectedResult)
-            $VersionA = ConvertTo-AUVersion $A
-            $VersionB = ConvertTo-AUVersion $B
-            if ($ExpectedResult -eq '>' ) {
-                $VersionA | Should BeGreaterThan $VersionB
-            } elseif ($ExpectedResult -eq '<' ) {
-                $VersionA | Should BeLessThan $VersionB
-            } else {
-                $VersionA | Should Be $VersionB
+        Context 'Get-Version [-SemVer V1]' {
+            It 'parses "<value>"' -TestCases @(
+                @{Result = '4.28.0'                       ; Value = '4.28'}
+                @{Result = '2.5.0'                        ; Value = 'v2.5.0'}
+                @{Result = '4.0.3-Beta1'                  ; Value = '4.0.3Beta1'}
+                @{Result = '4.0.3-Beta1'                  ; Value = '4.0.3Beta.1'}
+                @{Result = '8.0.0-rc1'                    ; Value = 'v8.0-rc1'}
+                @{Result = '8.0.0-rc1'                    ; Value = 'v8.0-rc.1'}
+                @{Result = '1.61.0-beta0'                 ; Value = 'v1.61.0-beta0'}
+                @{Result = '1.61.0-beta0'                 ; Value = 'v1.61.0-beta.0'}
+                @{Result = '1.79.2.23166'                 ; Value = '1.79.2.23166'}
+                @{Result = '2.1.1-beta2'                  ; Value = 'Current version 2.1.1 beta 2.'}
+                @{Result = '2.1.1-beta2'                  ; Value = 'Current version 2.1.1 beta.2.'}
+                @{Result = '5.6.3-x86msi'                 ; Value = 'https://dl.airserver.com/pc32/AirServer-5.6.3-x86.msi'}
+                @{Result = '5.6.3'       ; Delimiter = '-'; Value = 'https://dl.airserver.com/pc32/AirServer-5.6.3-x86.msi'}
+                @{Result = '5.32.1.1'                     ; Value = 'https://strawberryperl.com/download/5.32.1.1/strawberry-perl-5.32.1.1-32bit.msi'}
+            ) { param([string] $Value, [string] $Delimiter, [string] $Result)
+                $res = Get-Version $Value -Delimiter $Delimiter
+                $res            | Should Not BeNullOrEmpty
+                $res.ToString() | Should Be $Result
             }
         }
-    }
-}
 
-Describe 'Get-Version' -Tag getversion {
-    InModuleScope AU {
-        $testCases = @(
-            @{Value = 'v01.02.03.04beta.01+xyz.01'; ExpectedVersion = '1.2.3.4'; ExpectedPrerelease = 'beta.1'; ExpectedBuildMetadata = 'xyz.01'}
-            @{Value = 'v01.02.03 beta 01 xyz 01 z'; ExpectedVersion = '1.2.3'  ; ExpectedPrerelease = 'beta.1'; ExpectedBuildMetadata = 'xyz.01'}
-            @{Value = 'v01.02.03 beta01 xyz01'    ; ExpectedVersion = '1.2.3'  ; ExpectedPrerelease = 'beta.1'; ExpectedBuildMetadata = 'xyz.01'}
-        )
-
-        It 'should parse a non strict version: <value>' -TestCases $testCases { param([string] $Value, [version] $ExpectedVersion, [string] $ExpectedPrerelease, [string] $ExpectedBuildMetadata)
-            $res = Get-Version $Value
-            $res | Should Not BeNullOrEmpty
-            $res.Version | Should Be $ExpectedVersion
-            $res.Prerelease | Should BeExactly $ExpectedPrerelease
-            $res.BuildMetadata | Should BeExactly $ExpectedBuildMetadata
+        Context 'Get-Version -SemVer V2' {
+            It 'parses "<value>"' -TestCases @(
+                @{Result = '4.28.0'                        ; Value = '4.28'}
+                @{Result = '2.5.0'                         ; Value = 'v2.5.0'}
+                @{Result = '4.0.3-Beta1'                   ; Value = '4.0.3Beta1'}
+                @{Result = '4.0.3-Beta.1'                  ; Value = '4.0.3Beta.1'}
+                @{Result = '8.0.0-rc1'                     ; Value = 'v8.0-rc1'}
+                @{Result = '8.0.0-rc.1'                    ; Value = 'v8.0-rc.1'}
+                @{Result = '1.61.0-beta0'                  ; Value = 'v1.61.0-beta0'}
+                @{Result = '1.61.0-beta.0'                 ; Value = 'v1.61.0-beta.0'}
+                @{Result = '1.79.2.23166'                  ; Value = '1.79.2.23166'}
+                @{Result = '2.1.1-beta.2'                  ; Value = 'Current version 2.1.1 beta 2.'}
+                @{Result = '2.1.1-beta.2'                  ; Value = 'Current version 2.1.1 beta.2.'}
+                @{Result = '5.6.3-x86.msi'                 ; Value = 'https://dl.airserver.com/pc32/AirServer-5.6.3-x86.msi'}
+                @{Result = '5.6.3'        ; Delimiter = '-'; Value = 'https://dl.airserver.com/pc32/AirServer-5.6.3-x86.msi'}
+                @{Result = '5.32.1.1'                      ; Value = 'https://strawberryperl.com/download/5.32.1.1/strawberry-perl-5.32.1.1-32bit.msi'}
+            ) { param([string] $Value, [string] $Delimiter, [string] $Result)
+                $res = Get-Version -SemVer V2 $Value -Delimiter $Delimiter
+                $res            | Should Not BeNullOrEmpty
+                $res.ToString() | Should Be $Result
+            }
         }
 
-        $testCases = @(
-            @{ExpectedResult = '5.4.9'    ; Delimiter = '-' ; Value = 'http://dl.airserver.com/pc32/AirServer-5.4.9-x86.msi'}
-            @{ExpectedResult = '1.24.0-beta.2'              ; Value = 'https://github.com/atom/atom/releases/download/v1.24.0-beta2/AtomSetup.exe'}
-            @{ExpectedResult = '2.4.0.24-beta'              ; Value = 'https://github.com/gurnec/HashCheck/releases/download/v2.4.0.24-beta/HashCheckSetup-v2.4.0.24-beta.exe'}
-            @{ExpectedResult = '2.0.9'                      ; Value = 'http://www.ltr-data.se/files/imdiskinst_2.0.9.exe'}
-            @{ExpectedResult = '17.6'     ; Delimiter = '-' ; Value = 'http://mirrors.kodi.tv/releases/windows/win32/kodi-17.6-Krypton-x86.exe'}
-            @{ExpectedResult = '0.70.2'                     ; Value = 'https://github.com/Nevcairiel/LAVFilters/releases/download/0.70.2/LAVFilters-0.70.2-Installer.exe'}
-            @{ExpectedResult = '2.2.0-1'                    ; Value = 'https://files.kde.org/marble/downloads/windows/Marble-setup_2.2.0-1_x64.exe'}
-            @{ExpectedResult = '2.3.2'                      ; Value = 'https://github.com/sabnzbd/sabnzbd/releases/download/2.3.2/SABnzbd-2.3.2-win-setup.exe'}
-            @{ExpectedResult = '1.9'      ; Delimiter = '-' ; Value = 'http://download.serviio.org/releases/serviio-1.9-win-setup.exe'}
-            @{ExpectedResult = '0.17.0'                     ; Value = 'https://github.com/Stellarium/stellarium/releases/download/v0.17.0/stellarium-0.17.0-win32.exe'}
-            @{ExpectedResult = '5.24.3.1'                   ; Value = 'http://strawberryperl.com/download/5.24.3.1/strawberry-perl-5.24.3.1-32bit.msi'}
-            @{ExpectedResult = '3.5.4'                      ; Value = 'https://github.com/SubtitleEdit/subtitleedit/releases/download/3.5.4/SubtitleEdit-3.5.4-Setup.zip'}
-            @{ExpectedResult = '1.2.3-beta.4'               ; Value = 'v 1.2.3 beta 4'}
-            @{ExpectedResult = '1.2.3-beta.3'               ; Value = 'Last version: 1.2.3 beta 3.'}
-        )
-
-        It 'should parse any non strict version: <value>' -TestCases $testCases { param($Value, $Delimiter, $ExpectedResult)
-            $version = Get-Version $Value -Delimiter $Delimiter
-            $version | Should Be ([AUVersion] $ExpectedResult)
-        }
-    }
-}
-
-Describe '[AUVersion]' -Tag getversion {
-    InModuleScope AU {
-        $testCases = @(
-            @{Type = 'string'   ; Value = '1.2'}
-            @{Type = 'string'   ; Value = '1.2-beta+03'}
-            @{Type = 'AUVersion'; Value = [AUVersion] '1.2'}
-            @{Type = 'AUVersion'; Value = [AUVersion] '1.2-beta+03'}
-            @{Type = 'version'  ; Value = [version] '1.2'}
-            @{Type = 'System.Text.RegularExpressions.Capture'; Value = [regex]::Match('1.2', '^(.+)$').Groups[1]}
-            @{Type = 'System.Text.RegularExpressions.Capture'; Value = [regex]::Match('1.2-beta+03', '^(.+)$').Groups[1]}
-        )
-
-        It 'converts from: [<type>] <value>' -TestCases $testCases { param($Value)
-            $version = [AUVersion] $Value
-            $version | Should Not BeNullOrEmpty
+        Context 'Get-Version -SemVer EnhancedV2' {
+            It 'parses "<value>"' -TestCases @(
+                @{Result = '4.28.0'                        ; Value = '4.28'}
+                @{Result = '2.5.0'                         ; Value = 'v2.5.0'}
+                @{Result = '4.0.3-Beta.1'                  ; Value = '4.0.3Beta1'}
+                @{Result = '4.0.3-Beta.1'                  ; Value = '4.0.3Beta.1'}
+                @{Result = '8.0.0-rc.1'                    ; Value = 'v8.0-rc1'}
+                @{Result = '8.0.0-rc.1'                    ; Value = 'v8.0-rc.1'}
+                @{Result = '1.61.0-beta.0'                 ; Value = 'v1.61.0-beta0'}
+                @{Result = '1.61.0-beta.0'                 ; Value = 'v1.61.0-beta.0'}
+                @{Result = '1.79.2.23166'                  ; Value = '1.79.2.23166'}
+                @{Result = '2.1.1-beta.2'                  ; Value = 'Current version 2.1.1 beta 2.'}
+                @{Result = '2.1.1-beta.2'                  ; Value = 'Current version 2.1.1 beta.2.'}
+                @{Result = '5.6.3-x86.msi'                 ; Value = 'https://dl.airserver.com/pc32/AirServer-5.6.3-x86.msi'}
+                @{Result = '5.6.3'        ; Delimiter = '-'; Value = 'https://dl.airserver.com/pc32/AirServer-5.6.3-x86.msi'}
+                @{Result = '5.32.1.1'                      ; Value = 'https://strawberryperl.com/download/5.32.1.1/strawberry-perl-5.32.1.1-32bit.msi'}
+            ) { param([string] $Value, [string] $Delimiter, [string] $Result)
+                $res = Get-Version -SemVer EnhancedV2 $Value -Delimiter $Delimiter
+                $res            | Should Not BeNullOrEmpty
+                $res.ToString() | Should Be $Result
+            }
         }
     }
 }
